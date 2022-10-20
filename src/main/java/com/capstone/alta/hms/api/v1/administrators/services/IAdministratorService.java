38a92482c7fe64b33f0bcfc563aba1d107ca6b93
netng <net.nandang@gmail.com>
2022-10-20 16:52:26 +0700

    feat(craeteNewAdministrator): adding create new administrator interface
    adding craeteNewAdministrator as contract interface for create method

--- src/main/java/com/capstone/alta/hms/api/v1/administrators/services/IAdministratorService.java
+++ src/main/java/com/capstone/alta/hms/api/v1/administrators/services/IAdministratorService.java
@@ -1,4 +1,11 @@
 package com.capstone.alta.hms.api.v1.administrators.services;
 
+import com.capstone.alta.hms.api.v1.administrators.dtos.AdministratorRequestDTO;
+import com.capstone.alta.hms.api.v1.administrators.dtos.AdministratorResponseDTO;
+import com.capstone.alta.hms.api.v1.core.dtos.BaseResponseDTO;
+
 public interface IAdministratorService {
+    BaseResponseDTO<AdministratorResponseDTO> craeteNewAdministrator(
+            AdministratorRequestDTO administratorRequestDTO);
+    
 }
