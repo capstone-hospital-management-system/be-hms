e95a3a7606644e08fa16bc04dd644e1283ac0715
netng <net.nandang@gmail.com>
2022-10-20 16:56:52 +0700

    feat(getAllAdministrator): adding get all administrator abstract method
    adding getAllAdministrators as interface for getting all administrators list

--- src/main/java/com/capstone/alta/hms/api/v1/administrators/services/IAdministratorService.java
+++ src/main/java/com/capstone/alta/hms/api/v1/administrators/services/IAdministratorService.java
@@ -3,9 +3,14 @@ package com.capstone.alta.hms.api.v1.administrators.services;
 import com.capstone.alta.hms.api.v1.administrators.dtos.AdministratorRequestDTO;
 import com.capstone.alta.hms.api.v1.administrators.dtos.AdministratorResponseDTO;
 import com.capstone.alta.hms.api.v1.core.dtos.BaseResponseDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IAdministratorService {
-    BaseResponseDTO<AdministratorResponseDTO> craeteNewAdministrator(
+    BaseResponseDTO<AdministratorResponseDTO> createNewAdministrator(
             AdministratorRequestDTO administratorRequestDTO);
+    BaseResponseDTO<List<AdministratorResponseDTO>> getAllAdministrators(Pageable pageable);
     
+
 }
