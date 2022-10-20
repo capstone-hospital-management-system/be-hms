58af9ea31d155f05a3c45ba99a225b2ba9441dba
nan <net.nandang@gmail.com>
2022-10-21 05:57:30 +0700

    feat(Clinic): add clinic class entity
    adding clinic entity as clinic entity class that use by jpa to genera
    te clinics table on database system

---
+++ src/main/java/com/capstone/alta/hms/api/v1/clinics/entities/Clinic.java
@@ -0,0 +1,38 @@
+package com.capstone.alta.hms.api.v1.clinics.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity(name = "Clinic")
+@Table(name = "clinics")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Clinic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 200)
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+}
