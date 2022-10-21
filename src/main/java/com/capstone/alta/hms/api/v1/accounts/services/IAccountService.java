d29c7c29bdb03b129f0da19a979179cf40f8f789
netng <net.nandang@gmail.com>
2022-10-21 15:14:22 +0700

    feat(IAccountService): add get all accounts method
    adding getAllAccounts abstract method for implementing on service class for c
    rud operations

--- src/main/java/com/capstone/alta/hms/api/v1/accounts/services/IAccountService.java
+++ src/main/java/com/capstone/alta/hms/api/v1/accounts/services/IAccountService.java
@@ -3,9 +3,11 @@ package com.capstone.alta.hms.api.v1.accounts.services;
 import com.capstone.alta.hms.api.v1.accounts.dtos.AccountRequestDTO;
 import com.capstone.alta.hms.api.v1.accounts.dtos.AccountResponseDTO;
 import com.capstone.alta.hms.api.v1.core.dtos.BaseResponseDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IAccountService {
-    BaseResponseDTO<AccountResponseDTO> createNewAccount(
-        AccountRequestDTO accountRequestDTO
-    );
+    BaseResponseDTO<AccountResponseDTO> createNewAccount(AccountRequestDTO accountRequestDTO);
+    BaseResponseDTO<List<AccountResponseDTO>> getAllAccounts(Pageable pageable);
 }
