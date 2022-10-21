5df62b1c55b7dc42a466007feee45f1af26377fe
netng <net.nandang@gmail.com>
2022-10-21 14:54:59 +0700

    feat(AccountRequestDTO): add class dto
    adding class AccountRequestDTO for transpiling data before sending to backend

---
+++ src/main/java/com/capstone/alta/hms/api/v1/accounts/dtos/AccountRequestDTO.java
@@ -0,0 +1,39 @@
+package com.capstone.alta.hms.api.v1.accounts.dtos;
+
+import com.capstone.alta.hms.api.v1.accounts.utils.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+public class AccountRequestDTO implements Serializable {
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @JsonProperty("register_date")
+    private Date registerDate;
+
+    @NotNull
+    private Role role;
+
+    @NotNull
+    @JsonProperty("id_card")
+    private String idCard;
+
+    private String others;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+}
