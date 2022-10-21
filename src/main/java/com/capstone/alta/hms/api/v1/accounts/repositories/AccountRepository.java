fa057bb81bc018242f53a2633f432cfcfe2d2d41
netng <net.nandang@gmail.com>
2022-10-21 14:39:53 +0700

    feat(AccountRepository): add findAll method pageable
    adding findAll method at AccountRepository with pagination support

--- src/main/java/com/capstone/alta/hms/api/v1/accounts/repositories/AccountRepository.java
+++ src/main/java/com/capstone/alta/hms/api/v1/accounts/repositories/AccountRepository.java
@@ -1,9 +1,12 @@
 package com.capstone.alta.hms.api.v1.accounts.repositories;
 
 import com.capstone.alta.hms.api.v1.accounts.entities.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Page<Account> findAll(Pageable pageable);
 }
