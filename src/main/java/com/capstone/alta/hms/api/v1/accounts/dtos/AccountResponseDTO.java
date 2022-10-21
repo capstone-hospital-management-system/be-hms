866ac682e3821402b0a1ecabc3158340f183907b
netng <net.nandang@gmail.com>
2022-10-21 15:04:06 +0700

    feat(AccountResponseDTO): add account response dto
    adding AccountResponseDTO for transpilling data response from API before send to client

---
+++ src/main/java/com/capstone/alta/hms/api/v1/accounts/dtos/AccountResponseDTO.java
@@ -0,0 +1,35 @@
+package com.capstone.alta.hms.api.v1.accounts.dtos;
+
+import com.capstone.alta.hms.api.v1.accounts.utils.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+public class AccountResponseDTO implements Serializable {
+    private int id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @JsonProperty("register_date")
+    private Date registerDate;
+
+    private Role role;
+
+    @JsonProperty("id_card")
+    private String idCard;
+
+    private String others;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+}
