8e0923f230f4a97fd1eafe14a11983bdf447a91b
nan <net.nandang@gmail.com>
2022-10-23 09:48:57 +0700

    feat(PatientRepository): add patient repository
    add patient repository as jpa repository

---
+++ src/main/java/com/capstone/alta/hms/api/v1/patients/repositories/PatientRepository.java
@@ -0,0 +1,8 @@
+package com.capstone.alta.hms.api.v1.patients.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository {
+}
