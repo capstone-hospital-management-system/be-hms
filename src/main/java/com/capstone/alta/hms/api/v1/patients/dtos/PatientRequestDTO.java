3621a405cf709ce9cbb258342422ef0becf9f831
nan <net.nandang@gmail.com>
2022-10-23 10:17:08 +0700

    feat(PatientRequestDTO): add patient request dto
    adding patient request dto for tranpiling data from request body before send to backend api

---
+++ src/main/java/com/capstone/alta/hms/api/v1/patients/dtos/PatientRequestDTO.java
@@ -0,0 +1,54 @@
+package com.capstone.alta.hms.api.v1.patients.dtos;
+
+import com.capstone.alta.hms.api.v1.accounts.entities.Account;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+public class PatientRequestDTO implements Serializable {
+    @JsonProperty("first_name")
+    @NotNull
+    private String firstName;
+
+    @JsonProperty("last_name")
+    @NotNull
+    private String lastName;
+
+    @JsonProperty("id_card")
+    @NotNull
+    private String idCard;
+
+    @NotNull
+    private short age;
+
+    @NotNull
+    private String gender; //change this to enum latter
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String city;
+
+    @JsonProperty("blood_type")
+    private String bloodType; //change this to enum latter
+
+    @JsonProperty("phone_number")
+    @NotNull
+    private String phoneNumber;
+
+    @JsonProperty("postal_code")
+    @NotNull
+    private String postalCode;
+
+    private String username;
+
+    @NotNull
+    private Date bod;
+}
