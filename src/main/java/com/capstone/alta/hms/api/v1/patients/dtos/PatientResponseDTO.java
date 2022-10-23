fadd71a0e64f3960bd4b16a20afae8fb85383fd2
nan <net.nandang@gmail.com>
2022-10-23 11:13:58 +0700

    feat(PatientResponseDTO): add patient response dto
    adding PatientResponseDTO for tranpiling data response from server before sending to client

---
+++ src/main/java/com/capstone/alta/hms/api/v1/patients/dtos/PatientResponseDTO.java
@@ -0,0 +1,43 @@
+package com.capstone.alta.hms.api.v1.patients.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+public class PatientResponseDTO implements Serializable {
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("id_card")
+    private String idCard;
+
+    private short age;
+
+    private String gender; //change this to enum latter
+
+    private String address;
+
+    private String city;
+
+    @JsonProperty("blood_type")
+    private String bloodType; //change this to enum latter
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    @JsonProperty("postal_code")
+    private String postalCode;
+
+    private String username;
+
+    private Date bod;
+}
