90597324d40863c1f54933415ac440ab6b8aa605
nan <net.nandang@gmail.com>
2022-10-23 10:42:17 +0700

    fix(PatientRepository): add patient model and id data type
    adding patient model and id data type to PatientRepository

--- src/main/java/com/capstone/alta/hms/api/v1/patients/repositories/PatientRepository.java
+++ src/main/java/com/capstone/alta/hms/api/v1/patients/repositories/PatientRepository.java
@@ -1,8 +1,9 @@
 package com.capstone.alta.hms.api.v1.patients.repositories;
 
+import com.capstone.alta.hms.api.v1.patients.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends JpaRepository {
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
 }
