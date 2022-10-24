63943b25728393050567760e01d082bcc24ec2f2
nan <net.nandang@gmail.com>
2022-10-24 10:49:53 +0700

    feat(PatientRepository): add find all pageable
    adding findAll method on PatientRepository with pageable

--- src/main/java/com/capstone/alta/hms/api/v1/patients/repositories/PatientRepository.java
+++ src/main/java/com/capstone/alta/hms/api/v1/patients/repositories/PatientRepository.java
@@ -1,9 +1,13 @@
 package com.capstone.alta.hms.api.v1.patients.repositories;
 
+import com.capstone.alta.hms.api.v1.accounts.entities.Account;
 import com.capstone.alta.hms.api.v1.patients.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+    Page<Patient> findAll(Pageable pageable);
 }
