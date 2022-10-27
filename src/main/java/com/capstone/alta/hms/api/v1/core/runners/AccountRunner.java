14cea973284a9e191a20c50916748bb88e3ebcd9
nan <net.nandang@gmail.com>
2022-10-27 09:17:17 +0700

    feat(AccountRunner): account runner
    adding AccountRunner for auto generate default administrator account at first run of application if account table is empty

---
+++ src/main/java/com/capstone/alta/hms/api/v1/core/runners/AccountRunner.java
@@ -0,0 +1,59 @@
+package com.capstone.alta.hms.api.v1.core.runners;
+
+import com.capstone.alta.hms.api.v1.accounts.dtos.AccountRequestDTO;
+import com.capstone.alta.hms.api.v1.accounts.entities.Account;
+import com.capstone.alta.hms.api.v1.accounts.repositories.AccountRepository;
+import com.capstone.alta.hms.api.v1.accounts.utils.Role;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(AccountRunner.class);
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Value("${DEFAULT_ADMIN_USERNAME}")
+    private String username;
+
+    @Value("${DEFAULT_ADMIN_EMAIL}")
+    private String email;
+
+    @Value("${DEFAULT_ADMIN_PASSWORD}")
+    private String password;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (accountRepository.findAll().isEmpty()) {
+            AccountRequestDTO newAccount = new AccountRequestDTO();
+
+            newAccount.setFirstName("administrator");
+            newAccount.setLastName("administrator");
+            newAccount.setUsername(username);
+            newAccount.setEmail(email);
+            newAccount.setPassword(passwordEncoder.encode(password));
+            newAccount.setRole(Role.ADMINISTRATOR);
+            newAccount.setIdCard("123456789");
+            newAccount.setPhoneNumber("0123456789");
+
+            accountRepository.save(modelMapper.map(newAccount, Account.class));
+
+            logger.info("Default administrator created");
+
+        }
+    }
+}
