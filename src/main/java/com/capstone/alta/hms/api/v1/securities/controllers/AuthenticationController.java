c186d372fcb16f173d2c56814b3c00e9ecb98595
nan <net.nandang@gmail.com>
2022-10-27 10:45:12 +0700

    fix(AuthenticationController): AuthenticationController
    adding cors

--- src/main/java/com/capstone/alta/hms/api/v1/securities/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/alta/hms/api/v1/securities/controllers/AuthenticationController.java
@@ -8,11 +8,9 @@ import com.capstone.alta.hms.api.v1.securities.services.IAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class AuthenticationController {
