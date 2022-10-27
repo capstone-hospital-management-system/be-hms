8f106583db3874a27fd700102963eb92cabf43c7
nan <net.nandang@gmail.com>
2022-10-27 15:39:35 +0700

    feat(WebMvcConfiguration): add cors configuration globally
    adding cors configuration globally via WebMvcConfiguration bean

---
+++ src/main/java/com/capstone/alta/hms/api/v1/core/configurations/WebMvcConfiguration.java
@@ -0,0 +1,31 @@
+package com.capstone.alta.hms.api.v1.core.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    /**
+     * @MAX_AGE in miliss
+     */
+    private final long MAX_AGE = 3600;
+
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedOriginPatterns("*")
+            .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+            .maxAge(MAX_AGE);
+    }
+
+
+
+
+}
