f1d50ba8d0f04e3318b3bbcfc9881c9fe838b964
nan <net.nandang@gmail.com>
2022-10-28 10:27:49 +0700

    feat(Appointment-crud): appoinments crud
    completing appointments crud

--- src/main/java/com/capstone/alta/hms/api/v1/appointments/services/AppointmentService.java
+++ src/main/java/com/capstone/alta/hms/api/v1/appointments/services/AppointmentService.java
@@ -1,5 +1,6 @@
 package com.capstone.alta.hms.api.v1.appointments.services;
 
+import com.capstone.alta.hms.api.v1.accounts.dtos.AccountResponseDTO;
 import com.capstone.alta.hms.api.v1.accounts.entities.Account;
 import com.capstone.alta.hms.api.v1.accounts.repositories.AccountRepository;
 import com.capstone.alta.hms.api.v1.appointments.dtos.AppointmentRequestDTO;
@@ -9,16 +10,23 @@ import com.capstone.alta.hms.api.v1.appointments.repositories.AppointmentResposi
 import com.capstone.alta.hms.api.v1.clinics.entities.Clinic;
 import com.capstone.alta.hms.api.v1.clinics.repositories.ClinicRepository;
 import com.capstone.alta.hms.api.v1.core.dtos.BaseResponseDTO;
+import com.capstone.alta.hms.api.v1.core.dtos.MetaResponseDTO;
+import com.capstone.alta.hms.api.v1.core.dtos.PageBaseResponseDTO;
 import com.capstone.alta.hms.api.v1.patients.dtos.PatientResponseDTO;
 import com.capstone.alta.hms.api.v1.patients.entities.Patient;
 import com.capstone.alta.hms.api.v1.patients.repositories.PatientRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AppointmentService implements IAppointmentService{
@@ -67,6 +75,93 @@ public class AppointmentService implements IAppointmentService{
             HttpStatus.CREATED,
             "successfully creating data",
             modelMapper.map(appointment, AppointmentResponseDTO.class)
+        );
+    }
+
+    @Override
+    public PageBaseResponseDTO<List<AppointmentResponseDTO>> getAllAppointments(Pageable pageable) {
+        Page<Appointment> appointments = appointmentRespository.findAll(pageable);
+
+        if (!appointments.isEmpty()) {
+            List<AppointmentResponseDTO> appointmentResponseDTOS = appointments.stream()
+                    .map(appointment -> modelMapper.map(appointment, AppointmentResponseDTO.class))
+                    .collect(Collectors.toList());
+
+            return new PageBaseResponseDTO<>(
+                "200",
+                HttpStatus.OK,
+                "successfully retrieving data",
+                appointmentResponseDTOS,
+                new MetaResponseDTO(
+                    appointments.getNumber() + 1,
+                    appointments.getSize(),
+                    appointments.getTotalPages(),
+                    appointments.getTotalElements()
+                )
+            );
+        }
+
+        return new PageBaseResponseDTO<>(
+            "200",
+            HttpStatus.OK,
+            "data is empty",
+            Collections.emptyList(),
+            new MetaResponseDTO(
+                appointments.getNumber() + 1,
+                appointments.getSize(),
+                appointments.getTotalPages(),
+                appointments.getTotalElements()
+            )
+        );
+    }
+
+    @Override
+    public BaseResponseDTO<AppointmentResponseDTO> getAppointmentDetails(Integer id) {
+        Appointment appointment = appointmentRespository.findById(id).get();
+        return new BaseResponseDTO<>(
+                "200",
+                HttpStatus.OK,
+                "success",
+                modelMapper.map(appointment, AppointmentResponseDTO.class)
+        );
+    }
+
+    @Override
+    public BaseResponseDTO<AppointmentResponseDTO> updateAppointment(
+        Integer id, AppointmentRequestDTO appointmentRequestDTO) {
+
+        Patient patient = patientRepository.findById(appointmentRequestDTO.getPatientId()).get();
+        Account doctor = accountRepository.findById(appointmentRequestDTO.getDoctorId()).get();
+        Clinic clinic = clinicRepository.findById(appointmentRequestDTO.getClinicId()).get();
+        Account created_by = accountRepository.findById(appointmentRequestDTO.getCreatedBy()).get();
+
+        Appointment newAppointment = new Appointment();
+        newAppointment.setAppointmentDate(appointmentRequestDTO.getAppointmentDate());
+        newAppointment.setPatient(patient);
+        newAppointment.setDoctor(doctor);
+        newAppointment.setClinic(clinic);
+        newAppointment.setCreatedBy(created_by);
+        newAppointment.setUpdatedBy(created_by);
+
+        Appointment updatedAppointment = appointmentRespository.save(newAppointment);
+
+        return new BaseResponseDTO<>(
+                "200",
+                HttpStatus.OK,
+                "successfully updating data",
+                modelMapper.map(updatedAppointment, AppointmentResponseDTO.class)
+        );
+    }
+
+    @Override
+    public BaseResponseDTO<AppointmentResponseDTO> deleteAppointment(Integer id) {
+        appointmentRespository.deleteById(id);
+
+        return new BaseResponseDTO<>(
+                "200",
+                HttpStatus.OK,
+                "successfully deleting data",
+                null
         );
     }
 }
--- src/main/java/com/capstone/alta/hms/api/v1/appointments/services/IAppointmentService.java
+++ src/main/java/com/capstone/alta/hms/api/v1/appointments/services/IAppointmentService.java
@@ -1,10 +1,21 @@
 package com.capstone.alta.hms.api.v1.appointments.services;
 
+import com.capstone.alta.hms.api.v1.accounts.dtos.AccountResponseDTO;
 import com.capstone.alta.hms.api.v1.appointments.dtos.AppointmentRequestDTO;
 import com.capstone.alta.hms.api.v1.appointments.dtos.AppointmentResponseDTO;
 import com.capstone.alta.hms.api.v1.core.dtos.BaseResponseDTO;
+import com.capstone.alta.hms.api.v1.core.dtos.PageBaseResponseDTO;
+import com.capstone.alta.hms.api.v1.patients.dtos.PatientRequestDTO;
+import com.capstone.alta.hms.api.v1.patients.dtos.PatientResponseDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IAppointmentService {
     BaseResponseDTO<AppointmentResponseDTO> createNewAppointment(
         AppointmentRequestDTO appointmentRequestDTO);
+    PageBaseResponseDTO<List<AppointmentResponseDTO>> getAllAppointments(Pageable pageable);
+    BaseResponseDTO<AppointmentResponseDTO> getAppointmentDetails(Integer id);
+    BaseResponseDTO<AppointmentResponseDTO> updateAppointment(Integer id, AppointmentRequestDTO appointmentRequestDTO);
+    BaseResponseDTO<AppointmentResponseDTO> deleteAppointment(Integer id);
 }
